87cfc3876d4225e270a9d4fdbc634083f05a27e6
Ari <arieliha@gmail.com>
2022-03-12 12:53:11 -0300

    se agrega botones en header, banner, foto de perfil, reseteo de formulario acerca de.

--- src/app/components/add-about-me/add-about-me.component.ts
+++ src/app/components/add-about-me/add-about-me.component.ts
@@ -31,15 +31,31 @@ export class AddAboutMeComponent implements OnInit {
 
   onSubmit(){
     if(this.fullname.length === 0){
-      alert("Please add a name!");
+      alert("Por favor agrega un nombre");
       return
     }
 
+    if(this.title.length === 0){
+      alert("Por favor agrega un título");
+      return
+    }
+    
+    if(this.description.length === 0){
+      alert("Por favor agrega una descripción");
+      return
+    }
     const {id, fullname, title, picfile, description} = this
     const newAboutMe = {id, fullname, title, picfile, description}
 
     this.onAddAboutMe.emit(newAboutMe);
+    this.resetForm();
 
     
   }
+resetForm() {
+  this.fullname = "";
+  this.title = "";
+  this.picfile = "";
+  this.description = "";  
+}
 }
